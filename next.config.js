62e6efe9fab52e7edb839f167927eaac23cdab7e
Habiboullah0 <habiboullahmedlemin7@gmail.com>
2025-05-12 19:19:30 +0000

    Initial commit

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const { i18n } = require("./next-i18next.config.js");
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // any other configurations
 };
 
